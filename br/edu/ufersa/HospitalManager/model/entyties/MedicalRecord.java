23ab95bc29b78989d083523d53db1f6500d59453
João Neto <joao.neto70445@alunos.ufersa.edu.br>
2026-04-25 15:26:56 -0300

    add Record class
    The Record class has been added to the project, along with its getters and setters, inheritance and methods.

---
+++ br/edu/ufersa/HospitalManager/model/entyties/MedicalRecord.java
@@ -0,0 +1,35 @@
+package br.edu.ufersa.HospitalManager.model.entyties;
+import java.time.LocalDate;
+
+public class MedicalRecord {
+    private LocalDate date;
+    private String observation;
+
+    // Constructor
+    public MedicalRecord(String observation) {
+        setDate();
+        setObservation(observation);
+    }
+
+    // Getters and Setters for date
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public void setDate() {
+        this.date = LocalDate.now(); // sets creation date
+    }
+
+    // Getters and Setters for observation
+    public String getObservation() {
+        return this.observation;
+    }
+
+    public void setObservation(String observation) {
+        if (observation == null || observation.trim().isEmpty()) {
+            throw new IllegalArgumentException("Medical record observation cannot be empty.");
+        } else {
+            this.observation = observation;
+        }
+    }
+}
