dfaa7510c2d4f5e55f7ad375a0ea2256b2cd25c9
Maspoly <maspolyfilho7@gmail.com>
2026-05-31 11:30:19 -0300

    Doctor Update

--- src/main/java/br/edu/ufersa/hospital_manager/model/DAO/DoctorDAO.java
+++ src/main/java/br/edu/ufersa/hospital_manager/model/DAO/DoctorDAO.java
@@ -8,155 +8,178 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import br.edu.ufersa.hospital_manager.model.entities.Doctor;
+import br.edu.ufersa.hospital_manager.util.Connector;
 
 public class DoctorDAO implements BaseDAO<Doctor> {
     
     
     private Connection connection;
+
+    public DoctorDAO() {
+        this.connection = Connector.getConnection();
+    }
     
-    public static final String INSERT_SQL = "INSERT INTO Doctor (name, cpf, adress, consultation_value, council_code) VALUES (?, ?, ?, ?, ?);";
-    public static final String  DELETE_SQL = "DELETE FROM Doctor WHERE id = ?;";
-    public static final String  UPDATE_SQL = "UPDATE Doctor SET name = ?, cpf = ?, adress = ?, consultation_value = ?, council_code = ? WHERE id = ?;";
-    public static final String  SELECT_ALL_SQL = "SELECT * FROM Doctor;";
-    public static final String  SELECT_BY_CPF_SQL = "SELECT * FROM Doctor WHERE cpf = ?;";
-    public static final String  SELECT_BY_ID_SQL = "SELECT * FROM Doctor WHERE id = ?;";
-    public static final String  SELECT_BY_NAME_SQL = "SELECT * FROM Doctor WHERE name = ?;";
-    public static final String  SELECT_BY_COURCIL_CODE_SQL = "SELECT * FROM Doctor WHERE council_code = ?;";
+    public static final String INSERT_SQL = "INSERT INTO doctor (name, cpf, adress, consultation_value, council_code) VALUES (?, ?, ?, ?, ?);";
+    public static final String  DELETE_SQL = "DELETE FROM doctor WHERE id = ?;";
+    public static final String  UPDATE_SQL = "UPDATE doctor SET name = ?, cpf = ?, adress = ?, consultation_value = ?, council_code = ? WHERE id = ?;";
+    public static final String  SELECT_ALL_SQL = "SELECT * FROM doctor;";
+    public static final String  SELECT_BY_CPF_SQL = "SELECT * FROM doctor WHERE cpf = ?;";
+    public static final String  SELECT_BY_ID_SQL = "SELECT * FROM doctor WHERE id = ?;";
+    public static final String  SELECT_BY_NAME_SQL = "SELECT * FROM doctor WHERE name = ?;";
+    public static final String  SELECT_BY_COUNCIL_CODE_SQL = "SELECT * FROM doctor WHERE council_code = ?;";
     
     
     @Override
     public void create(Doctor entity) throws SQLException {
-        try{
-            PreparedStatement ps = connection.prepareStatement(INSERT_SQL, PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setString(1, entity.getName());
-            ps.setString(2, entity.getCPF());
-            ps.setString(3, entity.getAddress());
-            ps.setFloat(4, entity.getConsultationValue());
-            ps.setString(5, entity.getCouncilCode());
-            ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                entity.setId(rs.getLong(1)); // set the generated ID back to the entity
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        
-    }
-    
-    @Override
-    public void delete(long id) throws SQLException {
-        try{
-            PreparedStatement ps = connection.prepareStatement(DELETE_SQL);
-            ps.setLong(1, id);
-            ps.executeUpdate();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        
-    }
-    
-    @Override
-    public ArrayList<Doctor> listAll() throws SQLException {
-        try{
-            Statement ps = connection.createStatement();
-            ResultSet rs = ps.executeQuery(SELECT_ALL_SQL);
-            ArrayList<Doctor> doctors = new ArrayList<>();
-            while (rs.next()) {
-                Doctor doctor = new Doctor(rs.getString("name"), rs.getString("cpf"), rs.getString("adress"), rs.getFloat("consultation_value"), rs.getString("council_code"));
-                doctor.setId(rs.getLong("id"));
-                doctors.add(doctor);
-            }
-            return doctors;
-        }catch (SQLException e){
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
-    }
-    
-    @Override
-    public void update(Doctor entity) throws SQLException {
-        try{
-            PreparedStatement ps = connection.prepareStatement(UPDATE_SQL);
-            ps.setString(1, entity.getName());
-            ps.setString(2, entity.getCPF());
-            ps.setString(3, entity.getAddress());
-            ps.setFloat(4, entity.getConsultationValue());
-            ps.setString(5, entity.getCouncilCode());
-            ps.setLong(6, entity.getId());
-            ps.executeUpdate();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        
-    }
-    @Override
-    public Doctor readById(long id) throws SQLException {
-        try{
-            PreparedStatement ps = connection.prepareStatement(SELECT_BY_ID_SQL);
-            ps.setLong(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                Doctor doctor = new Doctor(rs.getString("name"), rs.getString("cpf"), rs.getString("adress"), rs.getFloat("consultation_value"), rs.getString("council_code"));
-                doctor.setId(rs.getLong("id"));
-                return doctor;
-            }
-            return null; // not found
-        }catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-    
-    public Doctor readByCPF(String cpf) throws SQLException {
-        try{
-            PreparedStatement ps = connection.prepareStatement(SELECT_BY_CPF_SQL);
-            ps.setString(1, cpf);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                Doctor doctor = new Doctor(rs.getString("name"), rs.getString("cpf"), rs.getString("adress"), rs.getFloat("consultation_value"), rs.getString("council_code"));
-                doctor.setId(rs.getLong("id"));
-                return doctor;
-            }
-            return null; // not found
-        }catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-    
-    
-    public Doctor readByName(String name) throws SQLException {
-        try{
-            PreparedStatement ps = connection.prepareStatement(SELECT_BY_NAME_SQL);
-            ps.setString(1, name);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                Doctor doctor = new Doctor(rs.getString("name"), rs.getString("cpf"), rs.getString("adress"), rs.getFloat("consultation_value"), rs.getString("council_code"));
-                doctor.setId(rs.getLong("id"));
-                return doctor;
-            }
-            return null; // not found
-        }catch (SQLException e){
-            e.printStackTrace();
-            return null;
+        PreparedStatement ps = connection.prepareStatement(INSERT_SQL, PreparedStatement.RETURN_GENERATED_KEYS);
+        ps.setString(1, entity.getName());
+        ps.setString(2, entity.getCPF());
+        ps.setString(3, entity.getAddress());
+        ps.setFloat(4, entity.getConsultationValue());
+        ps.setString(5, entity.getCouncilCode());
+        ps.executeUpdate();
+
+        ResultSet rs = ps.getGeneratedKeys();
+
+        if (rs.next()) {
+            entity.setId(rs.getLong(1)); // set the generated ID back to the entity
         }
     }
 
-    public Doctor readByCouncilCode(String councilCode) throws SQLException {
-        try{
-            PreparedStatement ps = connection.prepareStatement(SELECT_BY_COURCIL_CODE_SQL);
-            ps.setString(1, councilCode);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                Doctor doctor = new Doctor(rs.getString("name"), rs.getString("cpf"), rs.getString("adress"), rs.getFloat("consultation_value"), rs.getString("council_code"));
-                doctor.setId(rs.getLong("id"));
-                return doctor;
-            }
-            return null; // not found
-        }catch (SQLException e){
-            e.printStackTrace();
-            return null;
+    @Override
+    public void delete(Doctor entity) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(DELETE_SQL);
+        ps.setLong(1, entity.getId());
+        ps.executeUpdate();
+    }
+
+    @Override
+    public ArrayList<Doctor> listAll() throws SQLException {
+        Statement ps = connection.createStatement();
+        ResultSet rs = ps.executeQuery(SELECT_ALL_SQL);
+
+        ArrayList<Doctor> doctors = new ArrayList<>();
+
+        while (rs.next()) {
+            Doctor doctor = new Doctor(
+                    rs.getString("name"),
+                    rs.getString("cpf"),
+                    rs.getString("adress"),
+                    rs.getFloat("consultation_value"),
+                    rs.getString("council_code")
+            );
+
+            doctor.setId(rs.getLong("id"));
+            doctors.add(doctor);
         }
+
+        return doctors;
+    }
+
+    @Override
+    public void update(Doctor entity) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(UPDATE_SQL);
+
+        ps.setString(1, entity.getName());
+        ps.setString(2, entity.getCPF());
+        ps.setString(3, entity.getAddress());
+        ps.setFloat(4, entity.getConsultationValue());
+        ps.setString(5, entity.getCouncilCode());
+        ps.setLong(6, entity.getId());
+
+        ps.executeUpdate();
+    }
+
+    @Override
+    public Doctor readById(long id) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(SELECT_BY_ID_SQL);
+
+        ps.setLong(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            Doctor doctor = new Doctor(
+                    rs.getString("name"),
+                    rs.getString("cpf"),
+                    rs.getString("adress"),
+                    rs.getFloat("consultation_value"),
+                    rs.getString("council_code")
+            );
+
+            doctor.setId(rs.getLong("id"));
+            return doctor;
+        }
+
+        throw new SQLException("Doctor with ID " + id + " not found.");
+    }
+
+    public Doctor readByCPF(String cpf) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(SELECT_BY_CPF_SQL);
+
+        ps.setString(1, cpf);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            Doctor doctor = new Doctor(
+                    rs.getString("name"),
+                    rs.getString("cpf"),
+                    rs.getString("adress"),
+                    rs.getFloat("consultation_value"),
+                    rs.getString("council_code")
+            );
+
+            doctor.setId(rs.getLong("id"));
+            return doctor;
+        }
+
+        throw new SQLException("Doctor with CPF " + cpf + " not found.");
+    }
+
+    public Doctor readByName(String name) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(SELECT_BY_NAME_SQL);
+
+        ps.setString(1, name);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            Doctor doctor = new Doctor(
+                    rs.getString("name"),
+                    rs.getString("cpf"),
+                    rs.getString("adress"),
+                    rs.getFloat("consultation_value"),
+                    rs.getString("council_code")
+            );
+
+            doctor.setId(rs.getLong("id"));
+            return doctor;
+        }
+
+        throw new SQLException("Doctor with name " + name + " not found.");
+    }
+
+    public Doctor readByCouncilCode(String councilCode) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(SELECT_BY_COUNCIL_CODE_SQL);
+
+        ps.setString(1, councilCode);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            Doctor doctor = new Doctor(
+                    rs.getString("name"),
+                    rs.getString("cpf"),
+                    rs.getString("adress"),
+                    rs.getFloat("consultation_value"),
+                    rs.getString("council_code")
+            );
+
+            doctor.setId(rs.getLong("id"));
+            return doctor;
+        }
+
+        throw new SQLException("Doctor with council code " + councilCode + " not found.");
     }
 }
--- src/main/java/br/edu/ufersa/hospital_manager/model/entities/Doctor.java
+++ src/main/java/br/edu/ufersa/hospital_manager/model/entities/Doctor.java
@@ -1,29 +1,15 @@
 package br.edu.ufersa.hospital_manager.model.entities;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 // Doctor class inherits from Person
 public class Doctor extends Person {
     private float consultationValue;
     private String councilCode;
-    private Patient patient;
-    private ArrayList<Consultation> consultations = new ArrayList<>();
-    private ArrayList<MedicalRecord> medicalRecords = new ArrayList<>();
-    private ArrayList<String> reports = new ArrayList<>();
 
     // Constructor: initializes the doctor with basic data
     public Doctor(String name, String cpf, String address, float consultationValue, String councilCode) throws RuntimeException {
         super(name, cpf, address);
         setConsultationValue(consultationValue); // set consultation value
         setCouncilCode(councilCode); // validate and set council code
-
-        // Initialize attributes
-        this.patient = null;
-        setConsultations(new Consultation[100]); // fixed-size array for consultations
-        setMedicalRecords(new MedicalRecord[100]); // fixed-size array for medical records
-        setReports(new String[100]); // fixed-size array for reports
     }
 
     // Constructor: initializes the doctor with default consultation value
@@ -31,12 +17,7 @@ public class Doctor extends Person {
         super(name, cpf, address);
         setConsultationValue(100.0f); // set default consultation value
         setCouncilCode(councilCode); // validate and set council code
-
-        // Initialize attributes
-        this.patient = null;
-        setConsultations(new Consultation[100]); // fixed-size array for consultations
-        setMedicalRecords(new MedicalRecord[100]); // fixed-size array for medical records
-        setReports(new String[100]); // fixed-size array for reports
+    
     }
 
     // Getter for consultation value
@@ -67,190 +48,6 @@ public class Doctor extends Person {
         } else {
             this.councilCode = councilCode;
         }
-    }
-
-    // Getter for patient
-    public Patient getPatient() {
-        return this.patient;
-    }
-
-    // Setter with validation (cannot be null)
-    public void setPatient(Patient patient) throws RuntimeException {
-        if (patient == null) {
-            System.out.println("Patient cannot be null.");
-            throw new RuntimeException("Patient cannot be null.");
-        } else {
-            this.patient = patient;
-        }
-    }
-
-    // Getter for consultations array
-    public void getConsultations() {
-        for (Consultation c : consultations) {
-            if (c != null) {
-                System.out.println("Consultation: " + c.getDate() + " - " + c.getStatus());
-            }
-        }
-    }
-
-    // Setter with validation (cannot be null)
-    public void setConsultations(Consultation[] consultations){
-        if (consultations == null) {
-            System.out.println("Consultations cannot be null.");
-            throw new RuntimeException("Consultations cannot be null.");
-        } 
-        this.consultations.addAll(Arrays.asList(consultations)); // Take all the queries from the array and put them into the ArrayList.
-    }
-
-    // Getter for medical records array
-    public void getMedicalRecords() {
-        for (MedicalRecord c : medicalRecords) {
-            if (c != null) {
-                System.out.println("Medical Record: " + c.getDate() + " - " + c.getObservation());
-            }
-        }
-    }
-
-    // Setter with validation
-    public void setMedicalRecords(MedicalRecord[] medicalRecords) throws RuntimeException {
-        if (medicalRecords == null) {
-            System.out.println("Medical records cannot be null.");
-            throw new RuntimeException("Medical records cannot be null.");
-        } 
-        this.medicalRecords.addAll(Arrays.asList(medicalRecords)); // Take all the queries from the array and put them into the ArrayList.
-        }
-    
-
-    // Getter for reports
-    public void getReports() {
-        for (String c : reports) {
-            if (c != null) {
-                System.out.println("Report: " + c);
-            }
-        }
-    }
-
-    // Setter with validation (cannot be empty)
-    public void setReports(String[] reports) throws RuntimeException{
-        if (reports == null) {
-            System.out.println("Reports cannot be null.");
-            throw new RuntimeException("Reports cannot be null.");
-        } 
-        this.reports.addAll(Arrays.asList(reports)); // Take all the queries from the array and put them into the ArrayList.
-        }
-
-    // Method to register a medical record for a patient
-    public void registerMedicalRecord(Patient patient, MedicalRecord medicalRecord) throws RuntimeException {
-        if (patient == null || medicalRecord == null) {
-            System.out.println("Patient and medical record cannot be null.");
-            throw new RuntimeException("Patient and medical record cannot be null.");
-        } else {
-            patient.setMedicalRecord(medicalRecord); // associates record to patient
-        }
-    }
-
-    // Method to edit an existing medical record
-    public MedicalRecord editMedicalRecord(MedicalRecord medicalRecord, String newObs) throws RuntimeException {
-        if (medicalRecord == null || newObs == null || newObs.trim().isEmpty()) {
-            System.out.println("Invalid data.");
-            throw new RuntimeException("Invalid data.");
-        }
-
-        medicalRecord.setObservation(newObs); // update observation
-        return medicalRecord;
-    }
-
-    // Method to edit doctor's personal data
-    public void editPersonalData(String name, String cpf, String address, int consultationValue, String councilCode) throws RuntimeException {
-
-        // Validations
-        if (name == null || name.trim().isEmpty()) {
-            System.out.println("Name cannot be empty.");
-            throw new RuntimeException("Name cannot be empty.");
-        }
-        if (cpf == null || !cpf.matches("\\d{11}")) {
-            System.out.println("CPF must contain exactly 11 numeric digits");
-            throw new RuntimeException("CPF must contain exactly 11 numeric digits");
-        }
-        if (address == null || address.trim().isEmpty()) {
-            System.out.println("Address cannot be empty.");
-            throw new RuntimeException("Address cannot be empty.");
-        }
-        if (councilCode == null || councilCode.trim().isEmpty()) {
-            System.out.println("Council code cannot be empty.");
-            throw new RuntimeException("Council code cannot be empty.");
-        }
-
-        // Update data
-        setName(name);
-        setCPF(cpf);
-        setAddress(address);
-        setConsultationValue(consultationValue);
-        setCouncilCode(councilCode);
-    }
-
-    // Method to delete a medical record from the array
-    public void deleteMedicalRecord(MedicalRecord medicalRecord) throws RuntimeException {
-        if (medicalRecord == null) {
-            System.out.println("Medical record cannot be null.");
-            throw new RuntimeException("Medical record cannot be null.");
-        }
-
-        // Search and remove
-        for (MedicalRecord c : medicalRecords) {
-            if (c != null && c.equals(medicalRecord)) {
-                medicalRecords.remove(c); // remove the record from the list
-                System.out.println("Medical record deleted successfully.");
-                return;
-        } else{
-                System.out.println("Medical record not found.");
-            }
-        }
-    }
-
-    // Method to generate report based on date range
-    public void generateReport(LocalDate start, LocalDate end) throws RuntimeException {
-
-        if (start == null || end == null) {
-            throw new RuntimeException("Start and end dates cannot be null.");
-        }
-        if (end.isBefore(start)) {
-            throw new RuntimeException("End date cannot be before start date.");
-        }
-
-        int total = 0;
-        int scheduled = 0;
-        int completed = 0;
-        int canceled = 0;
-
-        // Iterate over consultations
-        for (Consultation c : consultations) {
-            if (c != null &&
-                (c.getDate().isEqual(start) || c.getDate().isAfter(start)) &&
-                (c.getDate().isEqual(end) || c.getDate().isBefore(end))) {
-
-                total++;
-
-                // Count by status
-                switch (c.getStatus()) {
-                    case "scheduled":
-                        scheduled++;
-                        return;
-                    case "completed":
-                        completed++;
-                        return;
-                    case "canceled":
-                        canceled++;
-                        return;
-                }
-            }
-        }
-
-        // Print results
-        System.out.println("Total: " + total);
-        System.out.println("Scheduled: " + scheduled);
-        System.out.println("Completed: " + completed);
-        System.out.println("Canceled: " + canceled);
     }
 }
 
